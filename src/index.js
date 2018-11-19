e21272fcad0b343945a9891ec3cfc14f1a283874
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2018-11-19 13:01:36 -0500

    cambios en html y pasando de index.js a cipher.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
-/*Creamos tres constantes, una en donde se va guardar el texto que ingrese el
- usuario, otra  enla que se va guardar el numero de codigo secreto y otra en la
- que se va guardar el nuevo texto*/
-let originalText;
-let secretCode;
-let newText;
-//Creamos una funcion que se tiene que ejecutar al presionar el botón cifrar
-function cifrar(){
-newText="";
 //Estamos obteniendo en la constante el elemento que se ingreso en el input "ingresartexto"
-  originalText = document.getElementById("ingresartexto");
-/*Estamos obteniendo el valor del elemento que se ingreso en el input
-"ingresartexto" y también en caso que el usuario ingrese el texto en minuscula, internammente
-se convertira en mayuscula*/
-  const originalTextValue = originalText.value.toUpperCase();
+let originalText = document.getElementById("ingresartexto");
 //Estamos obteniendo en la constante el elemento que se ingreso en el input "codigosecreto"
-  secretCode = document.getElementById("codigosecreto");
-//Estamos obteniendo el valor del elemento que se ingreso en el input "codigosecreto"
-  const secretCodeValue = parseInt(secretCode.value);
-//Creamos un bucle para obtener el valor ASCII de cada letra ingresada en el texto ABC DE
+let secretCode = document.getElementById("codigosecreto");
+//A la constante newText le damos el valor de cadena vacia
+let newText="";
+//Creamos una funcion que se tiene que ejecutar al presionar el botón cifrar
+function cipher(){
+  /*Estamos obteniendo el valor del elemento que se ingreso en el input
+  "ingresartexto" y también en caso que el usuario ingrese el texto en minuscula, internammente
+  se convertira en mayuscula*/
+    const originalTextValue = originalText.value.toUpperCase();
+  /*Estamos obteniendo el valor del elemento que se ingreso en el input
+  "codigosecreto" y tambien lo estamos convirtiendo en variable number*/
+    const secretCodeValue = parseInt(secretCode.value);
+//Creamos un bucle para obtener el valor ASCII de cada letra ingresada en el texto
   for (let i=0; i<originalTextValue.length; i++){
     let originalTextAscii = originalTextValue.charCodeAt(i);
-    console.log(originalTextAscii);
 /*Ponemos una condicional para que cuando se reconozca el espacio cuyo código
 ASCII es 32, éste sea transformado nuevamente a espacio y se concatene como espacio*/
-        if (originalTextAscii===32  ) {
+        if (originalTextAscii===32) {
       newText= newText + String.fromCharCode(originalTextAscii);
         }else{
           /*En caso no sea espacio, obtenemos en una nueva variable el codigo
@@ -38,20 +33,17 @@ ASCII es 32, éste sea transformado nuevamente a espacio y se concatene como esp
 }
 
 //Creamos una funcion que se tiene que ejecutar al presionar el botón descifrar
-function descifrar(){
-newText="";
-//Estamos obteniendo en la constante el elemento que se ingreso en el input "ingresartexto"
-  originalText = document.getElementById("ingresartexto");
-//Estamos obteniendo el valor del elemento que se ingreso en el input "ingresartexto"
-  const originalTextValue = originalText.value;
-//Estamos obteniendo en la constante el elemento que se ingreso en el input "codigosecreto"
-  secretCode = document.getElementById("codigosecreto");
-//Estamos obteniendo el valor del elemento que se ingreso en el input "codigosecreto"
-  const secretCodeValue = parseInt(secretCode.value);
+function descipher(){
+  /*Estamos obteniendo el valor del elemento que se ingreso en el input
+  "ingresartexto" y también en caso que el usuario ingrese el texto en minuscula, internammente
+  se convertira en mayuscula*/
+    const originalTextValue = originalText.value.toUpperCase();
+  /*Estamos obteniendo el valor del elemento que se ingreso en el input
+  "codigosecreto" y tambien lo estamos convirtiendo en variable number*/
+    const secretCodeValue = parseInt(secretCode.value);
 //Creamos un bucle para obtener el valor ASCII de cada letra ingresada en el texto ABC DE
   for (let i=0; i<originalTextValue.length; i++){
     let originalTextAscii = originalTextValue.charCodeAt(i);
-
 /*Ponemos una condicional para que cuando se reconozca el espacio cuyo código
 ASCII es 32, éste sea transformado nuevamente a espacio y se concatene como espacio*/
         if (originalTextAscii===32) {
