644a06890086e3dac56c2de15af7c59b99caf5c1
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2018-11-19 23:21:54 -0500

    mejorando funciones

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@
 let originalText = document.getElementById("enter-text");
 //Estamos obteniendo en la constante el elemento que se ingreso en el input "codigosecreto"
 let secretCode = document.getElementById("secret-code");
-//A la constante newText le damos el valor de cadena vacia
+//Creamosla variable newtex en donde aparecera el texto nuevo y para ello le damos el valor de cadena vacia
 let newText="";
+//Creamos una constante en la que se va obtener el elemento con id buttoncipher
+const buttonCipher=document.getElementById('button-cipher');
+//Creamos una constante en la que se va obtener el elemento con id buttondescipher
+const buttonDescipher=document.getElementById('button-descipher');
 //Creamos una funcion que se tiene que ejecutar al presionar el botón cifrar
- document.getElementById('button-cipher').addEventListener('click', () => {
-  /*Estamos obteniendo el valor del elemento que se ingreso en el input
-  "ingresartexto" y también en caso que el usuario ingrese el texto en minuscula, internammente
-  se convertira en mayuscula*/
-    const originalTextValue = originalText.value.toUpperCase();
-  /*Estamos obteniendo el valor del elemento que se ingreso en el input
-  "codigosecreto" y tambien lo estamos convirtiendo en variable number*/
-    const secretCodeValue = parseInt(secretCode.value);
+ buttonCipher.addEventListener('click', () => {
+/*Estamos obteniendo el valor del elemento que se ingreso en el input
+"ingresartexto" y también en caso que el usuario ingrese el texto en minuscula, internammente
+se convertira en mayuscula*/
+const originalTextValue = originalText.value.toUpperCase();
+/*Estamos obteniendo el valor del elemento que se ingreso en el input
+"codigosecreto" y tambien lo estamos convirtiendo en variable number*/
+const secretCodeValue = parseInt(secretCode.value);
 //Creamos un bucle para obtener el valor ASCII de cada letra ingresada en el texto
   for (let i=0; i<originalTextValue.length; i++){
     let originalTextAscii = originalTextValue.charCodeAt(i);
@@ -29,11 +33,11 @@ ASCII es 32, éste sea transformado nuevamente a espacio y se concatene como esp
         }
   }
   //Mostrar el nuevo texto en el input
-    document.getElementById("cipher-and-descipher-text").value = newText;
+    return document.getElementById("cipher-and-descipher-text").value = newText;
 });
 
 //Creamos una funcion que se tiene que ejecutar al presionar el botón descifrar
- document.getElementById('button-descipher').addEventListener('click', () => {
+ buttonDescipher.addEventListener('click', () => {
   /*Estamos obteniendo el valor del elemento que se ingreso en el input
   "ingresartexto" y también en caso que el usuario ingrese el texto en minuscula, internammente
   se convertira en mayuscula*/
@@ -57,5 +61,5 @@ ASCII es 32, éste sea transformado nuevamente a espacio y se concatene como esp
         }
   }
   //Mostrar el nuevo texto en el input
-    document.getElementById("cipher-and-descipher-text").value = newText;
+    return document.getElementById("cipher-and-descipher-text").value = newText;
 });
