7008a1005ea8dd089be654c5e31cd400dc0c70c1
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2018-11-22 19:32:54 -0500

    faltapruebadelinea38,40

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,12 +13,28 @@ describe('cipher', () => {
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
       assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG");
     });
+
+    it('debería retornar "Ó" para "Ñ" con offset 1' , () => {
+      assert.equal(cipher.encode(1,"Ñ"),"Ó");
+    });
+
+    it('debería retornar " " para " " con offset 1' , () => {
+      assert.equal(cipher.encode(1," ")," ");
+    });
   });
 
   describe('cipher.decode', () => {
 
     it('debería ser una función', () => {
       assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "Ñ" para "Ó" con offset 1' , () => {
+      assert.equal(cipher.decode(1,"Ó"),"Ñ");
+    });
+
+    it('debería retornar " " para " " con offset 1' , () => {
+      assert.equal(cipher.decode(1," ")," ");
     });
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33'); () => {
