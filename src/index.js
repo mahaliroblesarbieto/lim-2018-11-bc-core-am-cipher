d7b37c9c6e4e183234ff09fc0cc3782c10c574cb
Patty8909 <mahaliroblesarbieto@gmail.com>
2018-12-04 10:27:50 -0500

    cambiandofunctionaarrowfunctionyquitariddecss

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 const originalText  = document.getElementById("enter-text");
 const secretCode  = document.getElementById("secret-code");
-const buttonCipher  = document.getElementById('button-cipher');
-const buttonDescipher = document.getElementById('button-descipher');
-buttonCipher.addEventListener('click', hideMessage );
- //La función hideMessage sirve para mostrar el texto cifrado en el textarea
- function hideMessage(){
+ //Se agrega al boton cipher una función que sirve para mostrar el texto cifrado en el textarea
+document.getElementById('button-cipher').addEventListener('click', () => {
     const originalTextValue = originalText.value.toUpperCase();
     const secretCodeValue = parseInt(secretCode.value);
     let newText = cipher.encode(secretCodeValue, originalTextValue);
     document.getElementById("cipher-and-descipher-text").value = newText;
-  }
-buttonDescipher.addEventListener('click', discoverMessage)
- //La funcion discoverMessage sirve para mostrar el texto descifrado en el textarea
- function discoverMessage(){
+});
+   //Se agrega al boton descipher unaa funcion que sirve para mostrar el texto descifrado en el textarea
+document.getElementById('button-descipher').addEventListener('click', () => {
    const originalTextValue = originalText.value.toUpperCase();
    const secretCodeValue = parseInt(secretCode.value);
    let newText = cipher.decode(secretCodeValue, originalTextValue);
    document.getElementById("cipher-and-descipher-text").value = newText;
- }
+});
 //Agregamos la función recargar página
 document.getElementById('restart').addEventListener('click',() => {
   location.reload();
